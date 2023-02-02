8624979cb8d68110a962063baea5ed7da2888500
Iftekher Aziz <azizchy02@gmail.com>
2023-02-02 21:33:55 +0600

    Create quiz-21.js

---
+++ quiz-21.js
@@ -0,0 +1,37 @@
+// var x = Math.round(Math.random() * 10);
+// comsole.log(x);
+
+//  -------------
+// var a = 5; 
+// var b = 8; 
+// var c = 4; 
+// var d = 6; 
+// var highest = Math.max(a, b, c, d);
+// console.log(highest);
+
+// ------------
+//console.log(Math.floor(13.67));
+
+// ------------
+// const number = -78;
+// const answer = Math.abs(number);
+// console.log(answer);
+
+// ------------
+// var a = 2;
+// var b = 3;
+// if (a < b) {
+//   console.log('Hello');
+// }
+
+// ------------
+// function add(a,b){
+//     return a + b;
+// }
+// console.log(add(2,3));
+
+// ------------
+function add(a,b){
+    return a + b;
+}
+console.log(add('adam' + 'eve'));
